f671070a91b02305d3179cf6808321aeb86ff764
karlguo7 <karlguo@protonmail.com>
2021-05-11 15:12:27 +0800

    spike demo: init version

---
+++ src/main/java/cn/karlguo/spike/SpikeDemoApp.java
@@ -0,0 +1,20 @@
+package cn.karlguo.spike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*
+ * @Classname SpikeDemoApp
+ * @Date 2021/5/11
+ * @author karlguo
+ * @Description
+ */
+
+@SpringBootApplication
+public class SpikeDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpikeDemoApp.class, args);
+    }
+}
+
